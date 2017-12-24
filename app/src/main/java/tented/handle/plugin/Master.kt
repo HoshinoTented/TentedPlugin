e046ba5ff0a3618ca5db82a0dcbfd9de77f0444d
Hoshino Tented <limbolrain@gmail.com>
2017-12-25 05:29:26 +0800

    idea commit

--- app/src/main/java/tented/handle/plugin/Master.kt
+++ app/src/main/java/tented/handle/plugin/Master.kt
@@ -2,8 +2,11 @@ package tented.handle.plugin
 
 import com.saki.aidl.PluginMsg
 import com.saki.aidl.Type
+import tented.extra.getPath
 import tented.extra.times
 import tented.handle.MessageHandler
+import java.io.FileInputStream
+import java.util.Properties
 
 /**
  * Created by Hoshino Tented on 2017/12/25.
@@ -28,6 +31,21 @@ class Master : MessageHandler
             Main.list.add(name)
         }
 
+        fun getMasterList( group : Long ) : List<Any>
+        {
+            val file : java.io.File = java.io.File(tented.file.File.getPath("$group/Master.cfg"))
+
+            if( file.exists() )
+            {
+                val properties : Properties = Properties()
+
+                properties.load(FileInputStream(file))
+
+                return properties.keys.filter { properties.getProperty(it.toString(), "false") == "true" }
+            }
+
+            return arrayListOf()
+        }
 
     }
 
@@ -40,7 +58,17 @@ class Master : MessageHandler
 
         else if( msg.msg == "主人列表" )
         {
+            val list : List<Any> = getMasterList(msg.group)
+            val builder : StringBuilder = StringBuilder("主人列表如下\n${"-" * 9}")
 
+            for( element in list )
+            {
+                builder.append(">>$element<<\n")
+            }
+
+            builder.append("-" * 9)
+
+            msg.addMsg(Type.MSG, builder.toString())
         }
     }
 
